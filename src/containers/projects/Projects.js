7fbf58754dcabf59174a1a58f00b025baef95b01
Muhammad Faizan Asghar <bsce19021@itu.edu.pk>
2023-09-04 15:43:22 +0500

    portfolio-done

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -60,7 +60,7 @@ export default function Projects() {
             })}
           </div>
           <Button
-            text={"More Projects"}
+            text={"Github Projects"}
             className="project-button"
             href={socialMediaLinks.github}
             newTab={true}
--- src/portfolio.js
+++ src/portfolio.js
@@ -198,7 +198,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
@@ -259,7 +259,7 @@ const achievementSection = {
       title: "AWS Academy Graduate - AWS Academy Cloud Foundations",
       subtitle:
         "Certified AWS Academy Cloud Foundations Graduate 🎓",
-      image: require("./assets/images/cf.jpeg"),
+      image: require("./assets/images/cf.png"),
       imageAlt: "AWS Academy Graduate - AWS Academy Cloud Foundations",
       footerLink: [
         {
